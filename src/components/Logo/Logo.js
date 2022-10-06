294116c7a380c5bdfd055633c928bd052f517063
Carlos Zamora <czamora5@ucol.mx>
2022-10-06 17:42:25 -0500

    feat: add simple logo made out of divs

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Styles
+import './Logo.styles.scss';
+
+export default function Logo() {
+  return (
+    <div className="logo__wrapper">
+      <div className="logo"></div>
+    </div>
+  )
+}
