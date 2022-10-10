ef694310aaf9972cc314d92dadd4497e2bc90a08
Carlos Zamora <czamora5@ucol.mx>
2022-10-09 22:51:11 -0500

    feat: modify MovieCard component

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // Image
 import notFound from '../../assets/no-image.jpg';
@@ -7,19 +8,34 @@ import notFound from '../../assets/no-image.jpg';
 import './MovieCard.styles.scss';
 
 export default function MovieCard(props) {
-  let ratingColor;
+  const navigate = useNavigate();
+  let ratingColor, ratingTextColor;
 
   const rating = Number(props.rating);
-  if (rating <= 5) ratingColor = "255 0 0";
-  else if (rating <= 7.5) ratingColor = "255 255 0";
-  else ratingColor = "0 255 0";
+  if (rating <= 5) {
+    ratingColor = '255 20 20';
+    ratingTextColor = 'rgb(100, 50, 50)';
+  } else if (rating <= 7.5) {
+    ratingColor = '255 255 20';
+    ratingTextColor = 'rgb(150, 100, 20)';
+  } else {
+    ratingColor = '20 255 20';
+    ratingTextColor = 'rgb(50, 100, 50)';
+  }
 
   return (
-    <div className="card" style={{"--clr-rating": ratingColor}}>
-      <img className="card__image" src={props.image ?? notFound} alt={props.title} />
-      <div className="card__body">
-        <h3 className="card__title">{props.title}</h3>
-        <p className="card__rating">{props.rating}</p>
+    <div
+      onClick={() => navigate(`/movies/${props.id}`)}
+      className="movie-card"
+      style={{ "--clr-rating": ratingColor, "--clr-text-rating": ratingTextColor }}
+    >
+      <img className="movie-card__image" src={props.image ?? notFound} alt={props.title} />
+      <div className="movie-card__body">
+        <div className="card__info">
+          <h3 className="movie-card__title">{props.title}</h3>
+          <h4 className="movie-card__release">Release date: {props.release}</h4>
+        </div>
+        <p className="movie-card__rating">{props.rating.toFixed(1)}</p>
       </div>
     </div>
   );
