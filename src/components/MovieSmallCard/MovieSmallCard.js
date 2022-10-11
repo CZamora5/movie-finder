27fa23b4e5b6df722076c3694e957905797d9b5e
Carlos Zamora <czamora5@ucol.mx>
2022-10-11 10:01:41 -0500

    feat: add MovieSmallCard component which shows only the title and photo of a movie

---
+++ src/components/MovieSmallCard/MovieSmallCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+// Image
+import notFound from '../../assets/no-image.jpg';
+
+// Styles
+import './MovieSmallCard.styles.scss';
+
+export default function MovieSmallCard(props) {
+  const navigate = useNavigate();
+
+  return (
+    <div onClick={() => navigate(`/movies/${props.id}`)} className="movie-sm-card" >
+      <img className="movie-sm-card__image" src={props.image ?? notFound} alt={props.title} />
+      <div className="movie-sm-card__body">
+        <h3 className="movie-sm-card__title">{props.title}</h3>
+      </div>
+    </div>
+  );
+}
