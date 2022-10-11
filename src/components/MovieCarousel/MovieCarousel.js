f20fac2ee772d847179572f652a88128835c8e24
Carlos Zamora <czamora5@ucol.mx>
2022-10-11 12:55:38 -0500

    feat: add chevron icons to the MovieCarousel component

--- src/components/MovieCarousel/MovieCarousel.js
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -3,11 +3,21 @@ import React from 'react';
 //  Styles
 import './MovieCarousel.styles.scss';
 
+// Svgs
+import chevronLeft from '../../assets/chevron-left.svg';
+import chevronRight from '../../assets/chevron-right.svg';
+
 export default function MovieCarousel({ children }) {
   return (
     <div className="carousel">
       <div className="carousel__wrapper">
+        <div className="carousel__icon  carousel__icon--left">
+          <img className="carousel__svg" src={chevronLeft} alt="Flecha izquierda" />
+        </div>
         {children}
+        <div className="carousel__icon  carousel__icon--right">
+          <img className="carousel__svg" src={chevronRight} alt="Flecha derecha" />
+        </div>
       </div>
     </div>
   );
